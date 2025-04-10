36621626d21fa3bf8fb9cb5a85a2456d748fa50c
Kay <laurentia.kayleen@gmail.com>
2025-04-10 09:51:15 +0700

    Update Logging

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,18 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
+        Log.info("Starting WebDriver...");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        Log.info("Navigating to URL...");
         driver.get("https://admin-demo.nopcommerce.com/login");
     }
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
+            Log.info("Closing Broswer...");
             driver.quit();
         }
     }
--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import utils.Log;
+
 public class LoginPage {
     private final WebDriver driver;
 
@@ -15,16 +17,19 @@ public class LoginPage {
     }
 
     public void enterUsername(String username) {
+        Log.info("Entering username:" + username);
         driver.findElement(usernameTextBox).clear();
         driver.findElement(usernameTextBox).sendKeys(username);
     }
 
     public void enterPassword(String password) {
+        Log.info("Entering password: ********");
         driver.findElement(passwordTextBox).clear();
         driver.findElement(passwordTextBox).sendKeys(password);
     }
 
     public void clickLogin() {
+        Log.info("Clicking login button...");
         driver.findElement(loginButton).click();
     }
 } 
