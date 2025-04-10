3d83e1d48f14ddf67eafeeb8719a7971ffd3772f
Kay <laurentia.kayleen@gmail.com>
2025-04-10 11:51:33 +0700

    update

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 
@@ -13,14 +14,24 @@ public class LoginTest extends BaseTest {
     public void testValidLogin() {
         
         Log.info("Starting login test...");
+        test = ExtentReportManager.createTest("Login Test");
+        
+        test.info("Navigating to URL");
         LoginPage loginPage = new LoginPage(driver);
+        
+        test.info("Navigated to Login Page");
 
+        Log.info("Adding credentials");
+        test.info("Adding credentials");
         loginPage.enterUsername("admin@yourstore.com");
         loginPage.enterPassword("admin");
+        test.info("Clicking on Login button");
         loginPage.clickLogin();
+        test.info("Entered credentials and clicked Login");
         
-        System.out.println("Title of the page is : "+driver.getTitle());
         Log.info("Verifying page title...");
+        test.info("Verifying Page Title");
         Assert.assertEquals(driver.getTitle(), "Just a moment...");
+        test.pass("Login Successful");
     }
 }
