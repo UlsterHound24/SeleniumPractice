982ccf6dc56543378527789e94d3c2591f6a296d
Kay <laurentia.kayleen@gmail.com>
2025-04-11 11:07:54 +0700

    update excel

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,15 +1,33 @@
 package tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExcelUtils;
 import utils.ExtentReportManager;
 import utils.Log;
 
 
 public class LoginTest extends BaseTest {
+
+    public Object[][] getLoginData() throws IOException {
+        String filePath = System.getProperty("user.dir") + "/testdata/TestData.xlsx";
+        ExcelUtils.loadExcel(filePath, "Sheet1");
+        int rowCount = ExcelUtils.getRowCount();
+        Object[][] data = new Object[rowCount-1][2];
+        
+        for(int i = 1; i < rowCount; i++) {
+            data[i-1][0] = ExcelUtils.getCellData(i,0); //Username
+            data[i-1][1] = ExcelUtils.getCellData(i, 1); //Password 
+        }
+        ExcelUtils.closeExcel();
+        return data;
+    }
+
     @Test
     public void testValidLogin() {
         
